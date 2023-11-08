9edbd734534e12b19f44d74d65105051f7abd5f9
andazi <andaziboboye@gmail.com>
2023-11-08 23:57:42 +0100

    todo using classes

--- Learn the Fundamentals of Python Programming/number-guessing-game.py
+++ Learn the Fundamentals of Python Programming/number-guessing-game.py
@@ -1,23 +1,33 @@
 import random
 
 trials = 3
+# generating random number between 1 and 20
+rand_number = random.randint(1,20)    
+list_of_guesses = []
+#print(rand_number)
+while trials > 0:    
 
-while trials > 0:
-    # generating random number between 1 and 20
-    rand_number = random.randint(1,20)
-    print(rand_number)
-    # giving player 3 trials
-    for i in range(trials):
-        # input by default is a string
-        guess = int(input('Guess a number between from 1 to 20?      '))
-        if guess == rand_number:
-            break
+    guess = int(input('Guess a number between from 1 to 20?      '))
+
+    list_of_guesses.append(guess)
+    if guess == rand_number:
+        print('You have guessed correctly')
+        print('You WON')
+        # end while loop
+        trials = 0
+    else:
+        if guess > rand_number:
+            print('Number is too high')
         else:
-            if guess > rand_number:
-                print('Number is too high')
-            else:
-                print('Number is too low')
-        trials -= 1
-    if i < trials:
-        print('GAMEOVER, YOU LOSE')            
-    trials = 0
+            print('Number is too low')
+  
+    trials -= 1
+    if trials == 0:
+        print('GameOver, you LOSE')
+        
+print ("You guessed the following numbers: ", list_of_guesses)
+
+# we used the random module and called randint
+# randint method generates random interger
+# rand_number outside the while loop to avoid the rand_number from running again
+# while loop to track the number of times player would play
---
+++ Learn the Fundamentals of Python Programming/todo.py
@@ -0,0 +1,12 @@
+# create a list to contain the todos
+# create a function to add todos
+# create a function to remove todos
+# create a function to complete todos
+# create a function to add todos
+# create a list to show completed todos
+# create a function to clear list
+
+todos = []
+
+def add_todo(todo):
+    todos.append(todo)
