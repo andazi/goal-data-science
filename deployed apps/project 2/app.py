1706206cedbebbd121afe12909dd06a59b041724
andazi <andaziboboye@gmail.com>
2024-03-15 19:15:14 +0100

    cleaned date

--- deployed apps/project 2/app.py
+++ deployed apps/project 2/app.py
@@ -31,9 +31,17 @@ def load_data():
 
     # remove \xad
     df_agg.columns = df_agg.columns.str.replace('\xad','')
+
+    # drop Nan value
+    df_agg.dropna(inplace=True)
+
     # convert date to datetype
 
     df_agg['VIDEO PUBLISH TIME'] = pd.to_datetime(df_agg['VIDEO PUBLISH TIME'], format='mixed')
+
+    # rename column
+
+    df_agg.rename(columns={'VIDEO PUBLISH TIME': 'PUBLISH DATE', 'COMMENTS ADDED' : 'COMMENTS'}, inplace=True)
         
     df_agg['AVERAGE VIEW DURATION'] = df_agg['AVERAGE VIEW DURATION'].apply(lambda x: datetime.strptime(x, '%H:%M:%S'))
 
@@ -41,7 +49,7 @@ def load_data():
     df_agg['AVERAGE VIEW SECONDS'] = df_agg['AVERAGE VIEW DURATION'].apply(lambda x: x.second + x.minute * 60 + x.hour * 60 * 60)
 
     # engagement ration, every engagement a view could do divided by the number of viewrs
-    df_agg['ENGAGEMENT RATIO'] = (df_agg['SHARES'] + df_agg['LIKES'] + df_agg['DISLIKES'] + df_agg['COMMENTS ADDED']) / df_agg['VIEWS'] 
+    df_agg['ENGAGEMENT RATIO'] = (df_agg['SHARES'] + df_agg['LIKES'] + df_agg['DISLIKES'] + df_agg['COMMENTS']) / df_agg['VIEWS'] 
 
     # ratio of views to subscribers gained
     df_agg['VIEW TO SUBSCRIBER RATIO'] = df_agg['VIEWS'] / df_agg['SUBSCRIBERS GAINED'] # how views does it take to gain a subscriber
@@ -50,7 +58,7 @@ def load_data():
     df_agg['VIEW TO SUBSCRIBER LOST RATIO'] = df_agg['VIEWS'] / df_agg['SUBSCRIBERS LOST'] # how views does it take to lose a subscriber
 
     # sort data by 'VIDEO PUBLISH TIME'
-    df_agg.sort_values(by = 'VIDEO PUBLISH TIME', ascending=False, inplace=True)
+    df_agg.sort_values(by = 'PUBLISH DATE', ascending=False, inplace=True)
 
     # CONVERTING DATE to datetime
     df_vid['DATE'] = pd.to_datetime(df_vid['DATE'], format='mixed')
@@ -70,13 +78,14 @@ df_vid, df_agg, df_agg_sub, df_com = load_data()
 # create a copy of our dataframe
 df_agg_diff = df_agg.copy()
 
+
 # for the last 12 months, most recently date back to 12 months
-metric_date_12mo = df_agg_diff['VIDEO PUBLISH TIME'].max() - DateOffset(months=12)
+metric_date_12mo = df_agg_diff['PUBLISH DATE'].max() - DateOffset(months=12)
 
 # dataframe from metric_date_12mo to df_agg_diff['VIDEO PUBLISH TIME'].max()
 # that is, from 12 months early to current date
 
-df_agg_diff_12mo = df_agg_diff[df_agg_diff['VIDEO PUBLISH TIME'] >= metric_date_12mo]
+df_agg_diff_12mo = df_agg_diff[df_agg_diff['PUBLISH DATE'] >= metric_date_12mo]
 
 # median 
 median_agg = df_agg_diff_12mo[df_agg_diff_12mo.columns[2:]].median()
@@ -108,8 +117,8 @@ def styling_negative(value, props):
 if add_sidebar == "Aggregate Metrics":
 
     metric_agg = df_agg[[
-        'VIDEO PUBLISH TIME',
-        'COMMENTS ADDED', 
+        'PUBLISH DATE',
+        'COMMENTS', 
         'SHARES', 
         'DISLIKES', 
         'LIKES', 
@@ -124,8 +133,8 @@ if add_sidebar == "Aggregate Metrics":
 
     def metric_median(n):
         # date range
-        metric_date_n = metric_agg['VIDEO PUBLISH TIME'].max() - DateOffset(months=n)
-        median_date_n = metric_agg[metric_agg['VIDEO PUBLISH TIME'] >= metric_date_n].median()
+        metric_date_n = metric_agg['PUBLISH DATE'].max() - DateOffset(months=n)
+        median_date_n = metric_agg[metric_agg['PUBLISH DATE'] >= metric_date_n].median()
         
         return metric_date_n,median_date_n
 
@@ -138,9 +147,9 @@ if add_sidebar == "Aggregate Metrics":
     count = 0
     for i in median_6mo.index:
         with columns[count]:
-            if i != 'VIDEO PUBLISH TIME':
+            if i != 'PUBLISH DATE':
                 delta = (median_6mo[i] - median_12mo[i])/median_12mo[i]
-                st.metric(label = i, value =round(median_6mo[i]), delta="{:.2%}".format(delta))
+                st.metric(label = i, value =round(median_6mo[i]), delta="{:.4%}".format(delta))
             else:
                 delta = median_6mo[i] - median_12mo[i]
                 st.metric(label = 'Duration', value = delta.days, delta=f"{(delta//30)} Months")
@@ -152,8 +161,8 @@ if add_sidebar == "Aggregate Metrics":
     df_agg_diff_final = df_agg_diff.loc[:,[
     'VIDEO',
     'VIDEO TITLE',
-    'VIDEO PUBLISH TIME',
-    'COMMENTS ADDED',
+    'PUBLISH DATE',
+    'COMMENTS',
     'SHARES',
     'DISLIKES',
     'LIKES',
@@ -172,38 +181,48 @@ if add_sidebar == "Aggregate Metrics":
     ]
     
     # extract only date
-    df_agg_diff_final['VIDEO PUBLISH TIME'] = df_agg_diff_final['VIDEO PUBLISH TIME'].dt.date
-
-    # rename column
-
-    df_agg_diff_final.rename(columns={'VIDEO PUBLISH TIME': 'PUBLISH DATE'}, inplace=True)
+    df_agg_diff_final['PUBLISH DATE'] = df_agg_diff_final['PUBLISH DATE'].dt.date
 
     # extracting time
     df_agg_diff_final['AVERAGE VIEW DURATION'] = df_agg_diff_final['AVERAGE VIEW DURATION'].dt.time
 
+    # table
+    table_sidebar = st.sidebar.selectbox("Count or percentage", ("Count values", "Percentage value"))
     
-    if st.button('switch percentage'):
+    if table_sidebar != 'Count values':
 
         # Select only the numeric columns
         numeric_columns = df_agg_diff_final.select_dtypes(include=['number'])
 
+        df_agg_numeric_lst = df_agg_diff_final[df_agg_diff_final.columns[2:]].columns.tolist()
+        
+        #df_to_percent
+
         # Calculate column sums
         column_sums = numeric_columns.sum()
 
-        # Divide each element by its column sum and multiply by 100 to get percentages
-        df_percentage = (numeric_columns / column_sums) * 100
+        # Divide each element by its column sum and multiply by 100 to get percentages and round
+        #df_agg_diff_final = numeric_columns.applymap(lambda val: ((val / column_sums) * 100).round(4))
 
-        # Round the percentages to 2 decimal places
-        df_percentage = df_percentage.round(4)
+        df_to_percent = {}
+        for i in numeric_columns:
+            df_to_percent[i] = '{:.1%}'.format
+            
+
+
+        # Divide each element by its column sum and multiply by 100 to get percentages and round
+        #df_percentage = (((numeric_columns / column_sums) * 100).round(4)).astype(str) + '%'
+
+
 
         # Include the previously excluded non-numeric columns
-        non_numeric_columns = df_agg_diff_final.select_dtypes(exclude=['number'])
+        #non_numeric_columns = df_agg_diff_final.select_dtypes(exclude=['number'])
 
         # Concatenate numeric and non-numeric columns
-        df_agg_diff_final = pd.concat([non_numeric_columns, df_percentage], axis=1)
+        #df_agg_diff_final = pd.concat([non_numeric_columns, df_percentage], axis=1)
 
         
-        st.dataframe(df_agg_diff_final.style.hide().map(styling_positive, props = 'color:green;').map(styling_negative, props = 'color:red;'))
+        st.dataframe(df_agg_diff_final.style.hide().map(styling_positive, props = 'color:green;').map(styling_negative, props = 'color:red;').format(df_to_percent))
    
     else:
        # 
